27bc2e157a95eb7006bd2a907b5403d05700fb5b
Thomas.bausiere <t.bausiere@gmail.com>
2021-05-18 17:07:49 +0200

    adding files in project+ responsive website

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,15 +9,19 @@ const About = () => {
 
             {/* global div */}
             <div className="global">
+            
+            <div className="leftside">
+
             {/* Photo sur la gauche */}
-                <div className="photo">
+                
                     <img src="/img/Thomas2.jpg" alt="thomas"/>
-                </div>  
+               
+                </div>
             {/* Partie droite : texte    */}
             <div className="rightside">
                     {/* Premier encart en haut à droite */}
                     <div className="aboutme"> 
-                        <h1>A propos</h1>
+                        
                         <h1 className="h2">Thomas Bausière / 28 ans / Web Développeur</h1>
                         <p>Depuis toujours passioné d'informatique, j'ai décidé de me réorienter à mon 27ème anniversaire. <br/>
                         Je me suis tourné vers le centre de formation M2I pour apprendre les rudiments du code. Grâce à mes professeurs, j'ai appris bon nombre de choses, tel que le C# et le .Net.<br/>
@@ -25,25 +29,10 @@ const About = () => {
                         <p>Armé de determination pour devenir Web-Dev, je cherche maintenant une équipe pour m'accueillir.</p>
                     </div>
                     <hr/>
+
+                    {/* <div className="btn"><a href="C:\Users\Maz0\Desktop\cvo\src\ThomasBausiere_CV.pdf">Télécharger mon C.V.</a></div> */}
                     {/* Deuxième encart à en bas à droite */}
-                    <div className="myskills">
-                        <h3>Mes compétences</h3>
-                        <div className="skills">
-                            
-                                <h4>HTML</h4>
-                                <h4>Css/Sass</h4>
-                                <h4>Javascript</h4>
-                                <h4>React</h4>
-                                <h4>Docker</h4>                                
-                                <h4>GIT</h4>
-                                <h4>NodeJS</h4>
-                                <h4>C# (en cours d'apprentissage)</h4>                                
-                                <h4>.NET (en cours d'apprentissage)</h4>
-                                <h4> PHP (en cours d'apprentissage)</h4>
-                                
-                          
-                        </div>
-                    </div>
+
                 {/* <Contact/> */}
                 </div>
             </div>
--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -8,35 +8,79 @@ const Projets = () => {
             <Navigation/>
             
             {/* ARTICLE 1 */}
-            <div className="art1">
-                <div className="prez">
-                    <h1>CopyCats</h1>
-                    <p>Pour pouvoir m'entrainer à coder en HTML et CSS j'ai réalisé des copies de sites, en voici un exemple:
-                            
-                    </p>
-                    <a href="https://github.com/ThomasBausiere/CopyCat-Gw" class="btn">Voir sur GitHub</a>
+            <section className="articles">
+            <div className="article">
+                {/* partie Gauche*/}
+                <div className="left">
+                    <img src="/img/copycatgw.png" alt="ccgw"/>                    
                 </div>
-                <div className="vitrine1">
-                    <img src="/img/copycatgw.png" alt="ccgw"/>     
+
+                {/* partie droite*/}
+                <div className="right">
+
+                
+                <h1>Copycat-Gw</h1>
+                <p className="description">Alors que je découvrais L'HTML et le CSS, mon formateur m'a conseillé de m'entrainer et de refaire des pages internet.<br/> J'ai été voir un site que j'apprécie et je me suis entrainé. Voici le site de Guild Wars 1. <br/>Mon tout premier projets personnel!</p>
+                
+                    <a href="https://github.com/ThomasBausiere/CopyCat-Gw" class="btn" target="blank">Voir sur GitHub</a>
                 </div>
             </div>
                 {/* ARTICLE 2 */}
-            <div className="art2">            
-                <div className="vitrine2">
-                    <img src="/img/background_amel.png" alt="Amel"/>
-                </div>
-                <div className="prez2">
-                    <h1>Portfolio</h1>
-                    <p>Projet en cours de réalisation</p>
-                    <p>Amel Merzouk est une jeune graphiste à L'iscom, elle m'a demandé de réaliser son portfolio afin d'exposer ses créations.<br/>
-                    (Le site n'est pas encore en ligne, il arrivera bientot!!)
-                    </p>
-                    <a href="" class="btn">Accéder au site</a>
-                </div>
-                
-                    
-            </div>
             
+
+            <div className="article">            
+                <div className="left">
+                    <img src="/img/backgroundFC1.png" alt="FrenchCoder#Youtube"/>
+                </div>
+                <div className="right">               
+                <h1>Entrainement Responsive</h1>
+                <p className="description">Afin de pratiquer et de produire, j'ai commencé à réaliser des pages reponsives. Je me suis aidé de tutoriels sur youtube (#FrenchCoder) Le but était de m'entrainer et de découvrir certains aspects du mobile first que je n'avais pas encore pratiqué jusqu'alors.<br/>
+                Voici donc une page responsive d'un "A propos". Le texte est du Lorem Ipsum.</p>
+                
+                    <a href="https://github.com/ThomasBausiere/training_responsive" class="btn" target="blank">Voir sur GitHub</a>
+                </div>     
+            </div>
+
+            <div className="article">            
+                <div className="left">
+                    <img src="/img/backgroundFC2.png" alt="FrenchCoder#Youtube2"/>
+                </div>
+                <div className="right">
+                
+                <h1>Entrainement Responsive 2</h1>
+                <p className="description">Dans le même but que le projets précédent, je me suis entrainé en suivant l'un des tutos de French Coder. Cette fois ci, la page est une page d'accueil. </p>
+                
+                    <a href="https://github.com/ThomasBausiere/training_responsive2" class="btn" target="blank">Voir sur GitHub</a>
+                </div>     
+            </div>
+            <div className="article">            
+                <div className="left">
+                    <img src="/img/background_projet_react.png" alt="Thomas Bausiere"/>
+                </div>
+                <div className="right">
+                
+                <h1>Mon projet React !</h1>
+                <p className="description">Alors oui.. C'est bien d'utiliser la tech appropriée, et peut etre qu'une application React c'est un peu trop pour un site vitrine, mais je voulais faire du React pour apprendre, et progresser. Vous etes dessus, vous y naviguer. J'espere que vous appreciez !</p>
+                
+                    <a href="https://github.com/ThomasBausiere/PersonalWebSite" class="btn" target="blank">Voir sur GitHub</a>
+                </div>     
+            </div>
+
+            <div className="article">            
+                <div className="left">
+                    <img src="/img/background_amel.png" alt="AMZK"/>
+                </div>
+                <div className="right">
+                
+                <h1> [En cours de réalisation] Portfolio Amel Merzouk</h1>
+                <p className="description">Amel Merzouk est une étudiante en Graphisme et motion 3D à l'ISCOM Lille. Elle m'a demandé de réaliser son Portfolio afin d'y afficher ses travaux. La demande est précise, le challenge est élevé!
+                 Je travail sur ce site en ce moment, il n'est pas encore fini mais le sera prochainement </p>
+                
+                </div>
+            </div>
+
+
+            </section>
         </div>
 
         
