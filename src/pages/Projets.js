7a566e99a56f4b8f4f9047f009e06e61c062c50a
Thomas.bausiere <t.bausiere@gmail.com>
2021-05-19 16:19:32 +0200

    adding some responsive design on the project page

--- src/pages/Projets.js
+++ src/pages/Projets.js
@@ -20,7 +20,7 @@ const Projets = () => {
 
                 
                 <h1>Copycat-Gw</h1>
-                <p className="description">Alors que je découvrais L'HTML et le CSS, mon formateur m'a conseillé de m'entrainer et de refaire des pages internet.<br/> J'ai été voir un site que j'apprécie et je me suis entrainé. Voici le site de Guild Wars 1. <br/>Mon tout premier projets personnel!</p>
+                <p className="description">Alors que je découvrais L'HTML et le CSS, mon formateur m'a conseillé de m'entrainer et de refaire des pages internet.<br/> J'ai été voir un site que j'apprécie et je me suis entrainé. Voici le site de Guild Wars 1. <br/>Mon tout premier projet personnel !</p>
                 
                     <a href="https://github.com/ThomasBausiere/CopyCat-Gw" class="btn" target="blank">Voir sur GitHub</a>
                 </div>
@@ -34,7 +34,7 @@ const Projets = () => {
                 </div>
                 <div className="right">               
                 <h1>Entrainement Responsive</h1>
-                <p className="description">Afin de pratiquer et de produire, j'ai commencé à réaliser des pages reponsives. Je me suis aidé de tutoriels sur youtube (#FrenchCoder) Le but était de m'entrainer et de découvrir certains aspects du mobile first que je n'avais pas encore pratiqué jusqu'alors.<br/>
+                <p className="description">Afin de pratiquer et de produire, j'ai commencé à réaliser des pages reponsives. Je me suis aidé de tutoriels sur youtube (#FrenchCoder). Le but était de m'entrainer et de découvrir certains aspects du mobile first que je n'avais pas encore pratiqué jusqu'à présent.<br/>
                 Voici donc une page responsive d'un "A propos". Le texte est du Lorem Ipsum.</p>
                 
                     <a href="https://github.com/ThomasBausiere/training_responsive" class="btn" target="blank">Voir sur GitHub</a>
